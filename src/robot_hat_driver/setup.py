82490c13e99c6720a8a31c60145feff6836ce576
VioletSpace <johanna.pluschke@proton.me>
2026-06-02 13:32:17 +0200

    bringup and dummy description

--- src/robot_hat_driver/setup.py
+++ src/robot_hat_driver/setup.py
@@ -4,20 +4,20 @@ package_name = 'robot_hat_driver'
 
 setup(
  name=package_name,
- version='0.0.0',
+ version='0.1.0',
  packages=find_packages(exclude=['test']),
  data_files=[
      ('share/ament_index/resource_index/packages', ['resource/'+package_name]),
      ('share/'+package_name,           ['package.xml']),
-     ('share/'+package_name+"/launch", ["launch/robot_hat.launch.py"]),
+     ('share/'+package_name+"/launch", ["launch/robot_hat.launch.xml"]),
      ('share/'+package_name+"/config", ["config/robot_hat.yaml"]),
    ],
- install_requires=['setuptools', 'robot_hat'],
+ install_requires=['setuptools'],
  zip_safe=True,
  maintainer='Johanna Pluschke',
  maintainer_email='johanna.pluschke.ext@ptb.de',
  description='ROS 2 interface for SunFounder Robot HAT V3.3',
- license='MIT',
+ license='Apache-2.0',
  tests_require=['pytest'],
  entry_points={
      'console_scripts': [
