f5f9aa708ec48eee505e0e7200f888ed5596e0d2
VioletSpace <johanna.pluschke@proton.me>
2026-06-02 09:21:29 +0200

    update launch and config

--- src/robot_hat_driver/launch/robot_hat.launch.py
+++ src/robot_hat_driver/launch/robot_hat.launch.py
@@ -1,15 +1,16 @@
-from launch import LaunchDescription
+from launch import LaunchDescription, PathJoinSubstitution
 from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
     return LaunchDescription([
         Node(
             package="robot_hat_driver",
-            executable="robot_hat_node",
+            executable="rhdriver",
             name="robot_hat_node",
             parameters=[
-                "config/robot_hat.yaml"
+                PathJoinSubstitution([FindPackageShare('robot_hat_driver'), 'config', 'robot_hat.yaml'])
             ],
         )
     ])
--- src/robot_hat_driver/setup.py
+++ src/robot_hat_driver/setup.py
@@ -7,13 +7,10 @@ setup(
  version='0.0.0',
  packages=find_packages(exclude=['test']),
  data_files=[
-     ('share/ament_index/resource_index/packages',
-             ['resource/' + package_name]),
-     ('share/' + package_name, ['package.xml']),
-     ("share/" + package_name + "/launch",
-             ["launch/robot_hat.launch.py"]),
-     ("share/" + package_name + "/config",
-             ["config/robot_hat.yaml"]),
+     ('share/ament_index/resource_index/packages', ['resource/'+package_name]),
+     ('share/'+package_name,           ['package.xml']),
+     ('share/'+package_name+"/launch", ["launch/robot_hat.launch.py"]),
+     ('share/'+package_name+"/config", ["config/robot_hat.yaml"]),
    ],
  install_requires=['setuptools', 'robot_hat'],
  zip_safe=True,
@@ -24,7 +21,7 @@ setup(
  tests_require=['pytest'],
  entry_points={
      'console_scripts': [
-             'robot_hat_node = robot_hat_driver.robot_hat_node:main'
+             'rhdriver = robot_hat_driver.robot_hat_node:main'
      ],
    },
 )
