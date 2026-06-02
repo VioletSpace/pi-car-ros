ed30cc9439d6b38ec882f0ba5806fdc00c983b0b
VioletSpace <johanna.pluschke@proton.me>
2026-06-02 16:59:39 +0200

    add controller package

---
+++ src/picar_controller/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+package_name = 'picar_controller'
+
+setup(
+    name=package_name,
+    version='0.1.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/'+package_name]),
+        ('share/'+package_name,           ['package.xml']),
+        ('share/'+package_name+"/launch", ["launch/robot_hat.launch.xml"]),
+        ('share/'+package_name+"/config", ["config/robot_hat.yaml"]),
+      ],
+    package_data={'': ['py.typed']},
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Johanna Pluschke',
+    maintainer_email='johanna.pluschke.ext@ptb.de',
+    description='ROS 2 main controller for PiCar-X using Sunfounder Robot Hat V3.3',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
