cf59e939406db38ace2c470773a313bd55ae6dd8
Johanna Freya Pluschke <johanna.pluschke.ext@ptb.de>
2026-06-22 10:49:04 +0200

    fix hardware crash avoidance

--- src/robot_hat_driver/robot_hat_driver/hardware.py
+++ src/robot_hat_driver/robot_hat_driver/hardware.py
@@ -14,22 +14,22 @@ class RobotHatHardware:
         self.logger = logger
         self.params = params
 
-        utils.reset_mcu() # Needed to reset broken states at boot
-        time.sleep(0.2)
-
         # Sanity checks for hardware availability
         if not os.path.exists("/dev/i2c-1"):
             self.logger.error("I2C bus missing (/dev/i2c-1)  - incorrect environment")
             raise RuntimeError("I2C bus not available")
-        if not os.path.exists("/dev/gpiomem") and not os.path.exists("/dev/gpiochip0"):
+        if not (os.path.exists("/dev/gpiomem") and os.path.exists("/dev/gpiochip0")):
             self.logger.error("GPIO interface missing (/dev/gpiomem or /dev/gpiochip0) - incorrect environment")
             raise RuntimeError("GPIO not available")
         
         # Initialise hardware
+        utils.reset_mcu() # Needed to reset broken states at boot
+        time.sleep(0.2)
+        
         self.motors = Motors(params["lmid"], params["rmid"], params["lmrev"], params["rmrev"])
         try: self.servos = [Servo(ch) for ch in params["servo_channels"]]
         except ValueError as err:
-            self.logger.err("Could not initialise Servos: " + err)
+            self.logger.error(f"Could not initialise Servos: {err}")
             self.servos = []
         else: self.logger.info("Servos available")
         self.battery_adc = ADC("A4")
@@ -43,7 +43,7 @@ class RobotHatHardware:
         if params['us_s']:
             try: self.ultrasonic = Ultrasonic(Pin(params["us_pins"][0]), Pin(params["us_pins"][1], mode=Pin.IN, pull=Pin.PULL_DOWN))
             except ValueError as err:
-                self.logger.err(f"Could not initialise Sonar sensor, deactivating: {err}")
+                self.logger.error(f"Could not initialise Sonar sensor, deactivating: {err}")
                 self.params['us_s'] = False
             else: self.logger.info("Sonar sensor available")
         if params['gs_s']:
@@ -51,7 +51,7 @@ class RobotHatHardware:
                 adc0, adc1, adc2 = [ADC(pin) for pin in params['gs_pins']]
                 self.grayscale = Grayscale_Module(adc0, adc1, adc2, reference=None)
             except ValueError as err:
-                self.logger.err(f"Could not initialise Grayscale sensor, deactivating: {err}")
+                self.logger.error(f"Could not initialise Grayscale sensor, deactivating: {err}")
                 self.params['gs_s'] = False
             else: self.logger.info("Grayscale sensor available")
 
@@ -72,13 +72,14 @@ class RobotHatHardware:
         self.motors.right.speed(right)
 
     def stop(self):
-        """Set motors to 0"""
+        """Set motors to 0, resets"""
         self.set_motor_speeds(0.0, 0.0)
+        utils.reset_mcu()
 
     def set_servo(self, channel: int, angle: float):
         """Set the servo at channel to angle. Applies corrections defined in the `servo_correction` ROS2 parameter"""
         if channel < len(self.servos) and channel >= 0:
-            self.servos[channel].angle(angle - self.params["servo_corr"][channel])
+            self.servos[channel].angle(angle + self.params["servo_corr"][channel])
         else:
             self.logger.warn(f"Invalid servo index {channel} of {len(self.servos)}, ignoring.")
 
