2b5b6058fba3f3f1a9d0b8f94840c740cc5ef378
Johanna Freya Pluschke <johanna.pluschke.ext@ptb.de>
2026-06-22 14:01:58 +0200

    fix install.sh

--- src/picar_steamdeck/install.sh
+++ src/picar_steamdeck/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cp /home/deck/ros2_ws/src/picar_controller/gui/controller.desktop /home/deck/Desktop/
-cp /home/deck/ros2_ws/src/picar_controller/gui/kill.desktop /home/deck/Desktop/
+cp /home/deck/ros2_ws/src/picar_steamdeck/gui/controller.desktop /home/deck/Desktop/
+cp /home/deck/ros2_ws/src/picar_steamdeck/gui/kill.desktop /home/deck/Desktop/
 chmod +x /home/deck/Desktop/controller.desktop
 chmod +x /home/deck/Desktop/kill.desktop
